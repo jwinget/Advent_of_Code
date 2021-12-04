4c2434ebca45f3ac9ca91d6b95a2a983859962ef
Jason Winget <jwinget@gmail.com>
2021-12-03 22:04:23 -0500

    Day 3 part 2

--- 2021/3.R
+++ 2021/3.R
@@ -21,7 +21,7 @@ input <- read_csv(here('2021', 'd3_input.txt'),
                   col_names = c("diag"))
 
 
-#---- solution ----
+#---- part 1 ----
 output <- function(d) {
   max_len <- max(nchar(d$diag))
   
@@ -65,3 +65,71 @@ output <- function(d) {
 
 output(example)
 output(input)
+
+
+#---- part 2 ----
+check_bit <- function(vec, flip = FALSE, position = 1) {
+  even <- FALSE
+  if (sum(as.numeric(vec)) > length(vec)/2) {
+    # More ones
+    res <- 1
+  } else if (sum(as.numeric(vec)) < length(vec)/2) {
+    # More zeros
+    res <- 0
+  } else {
+    # Count is even, return the positional value
+    res <- 1
+  }
+  if (flip) {return(as.numeric(!res))}
+  return(res)
+}
+split_data <- function(d) {
+  max_len <- max(nchar(d$diag))
+  cols <- letters[1:max_len]
+  return(d %>%
+    separate(diag, into = cols, sep = "(?<=.)"))
+}
+
+get_readout <- function(split_d, flip) {
+  d <- split_d
+  res <- c()
+  j <- 0
+  for(i in 1:ncol(split_d)) {
+    # Determine which bit to keep
+    check <- check_bit(d[[letters[i]]], flip = flip, position = i-j)
+    j <- j+1
+    # Append it to the result
+    res <- c(res, check)
+    
+    d <- d %>%
+      filter(.data[[letters[i]]] == check)
+    
+    if (nrow(d) == 1) {
+      # Solution is found, append the last value and return it
+      print(res)
+      print(j)
+      print(d)
+      if(ncol(d) > 1) {
+        
+        d <- d %>%
+          select(-.data[[letters[i]]])
+        bstr <- c(res, d[1,])
+      }
+      else {
+        bstr <- res
+      }
+      binary <- stringr::str_c(bstr, collapse = "")
+      return(strtoi(binary, base = 2))
+    } else {
+      # Drop the first bit
+      d <- d %>%
+        select(-.data[[letters[i]]])
+    }
+  }
+}
+
+d <- split_data(input)
+ogr <- get_readout(d, flip = FALSE)
+csr <- get_readout(d, flip = TRUE)
+
+ogr*csr

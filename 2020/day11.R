229cc8aa5075f24bd0999948e566a8b15ceacb00
Jason Winget <jwinget@gmail.com>
2021-12-06 19:10:54 -0500

    Day 6

--- 2020/day11.R
+++ 2020/day11.R
@@ -10,193 +10,50 @@ library(tidyverse)
 
 # Read data ---------------------------------------------------------------
 
-d <- readLines(here("data", glue("day_{Day}_input.txt")))
+d <- readLines(here("2020", "data", glue("day_{Day}_input.txt")))
 dt <- c("L.LL.LL.LL", "LLLLLLL.LL", "L.L.L..L..", "LLLL.LL.LL",
         "L.LL.LL.LL", "L.LLLLL.LL", "..L.L.....", "LLLLLLLLLL",
         "L.LLLLLL.L", "L.LLLLL.LL")
 
 # Functions ---------------------------------------------------------------
 
-find_neighbors <- function(d, mode) {
-  width <- nchar(d[1])
-  height <- length(d)
-  
-  # Set up the data structure
-  df <- tibble(pos = unlist(strsplit(d, "")),
-               neighbors = rep(NA, sum(nchar(d)))) %>%
-    mutate(neighbors = as.list(neighbors),
-           pos = case_when(
-             pos == "L" ~ FALSE,
-             pos == "." ~ NA
-           ))
-  
-  # Find the neighbors of each seat
-  # Store their row indices as a vector in "neighbors"
-  for(i in seq(1:nrow(df))){
-      possible_indices <- list(N = i - width,
-                            NE = i - width + 1,
-                            E = i + 1,
-                            SE = i + width + 1,
-                            S = i + width,
-                            SW = i + width - 1,
-                            W = i - 1,
-                            NW = i - width - 1
-                            )
-      
-      # Deal with left and right edges
-      if (i %% width == 0) {
-        # End of a row
-        possible_indices$E <- NULL
-        possible_indices$NE <- NULL
-        possible_indices$SE <- NULL
-      } else if (i %% width == 1) {
-        # Start of a row
-        possible_indices$W <- NULL
-        possible_indices$NW <- NULL
-        possible_indices$SW <- NULL
-      }
-      
-      # Deal with top and bottom edges
-      if (i <= width) {
-        # Top row
-        possible_indices$N <- NULL
-        possible_indices$NE <- NULL
-        possible_indices$NW <- NULL
-      } else if (i >= width * height) {
-        # Bottom row
-        possible_indices$S <- NULL
-        possible_indices$SE <- NULL
-        possible_indices$SW <- NULL
-      }
-      
-      if (mode == "visible") {
-        for (pi in names(possible_indices)) {
-        j <- 2
-        while (is.na(df[possible_indices[[pi]],]$pos)) {
-          if (pi == "N") {
-            ti <- i - width*j
-            if (ti > 0 & !is.na(df[ti,]$pos)) {
-              possible_indices[[pi]] <- ti
-              break
-            } else if (is.na(df[ti,]$pos)) {
-              j <- j + 1
-            } else if (ti <= 0) {
-              break
-            }
-          } else if (pi == "NE") {
-            ti = i - width*j + 1*j
-            if (ti > 0 & !is.na(df[ti,]$pos)) {
-              possible_indices[[pi]] <- ti
-              break
-            } else if (is.na(df[ti,]$pos)) {
-              j <- j + 1
-            } else if (ti <= 0) {
-              break
-            }
-          } else if (pi == "E") {
-            while(j <= width - i) {
-            ti = i + j
-            if (!is.na(df[ti,]$pos)) {
-              possible_indices[[pi]] <- ti
-              break
-            } else { j <- j + 1}
-            }
-          } else if (pi == "SE") {
-            ti = i + width*j + 1*j
-            if (ti < width * height & !is.na(df[ti,]$pos)) {
-              possible_indices[[pi]] <- ti
-              break
-            } else if (is.na(df[ti,]$pos)) {
-              j <- j + 1
-            } else if (ti >= width * height) {
-              break
-            }
-          } else if (pi == "S") {
-            ti = i + width*j
-            if (ti < width * height & !is.na(df[ti,]$pos)) {
-              possible_indices[[pi]] <- ti
-              break
-            } else if (is.na(df[ti,]$pos)) {
-              j <- j + 1
-            } else if (ti >= width * height) {
-              break
-            }
-            
-          } else if (pi == "SW") {
-            ti = i + width*j - 1*j
-            if (ti < width * height & !is.na(df[ti,]$pos)) {
-              possible_indices[[pi]] <- ti
-              break
-            } else if (is.na(df[ti,]$pos)) {
-              j <- j + 1
-            } else if (ti >= width * height) {
-              break
-            }
-          } else if (pi == "W") {
-            ti = i - j
-            if (!is.na(df[ti,]$pos)) {
-              possible_indices[[pi]] <- ti
-              break
-            } else { j <- j + 1}
-          }
-          else if (pi == "NW") {
-            ti = i - width*j - 1*j
-            if (ti > 0 & !is.na(df[ti,]$pos)) {
-              possible_indices[[pi]] <- ti
-              break
-            } else if (is.na(df[ti,]$pos)) {
-              j <- j + 1
-            } else if (ti <= 0) {
-              break
-            }
-          }
-        }
-        }
-      }
-      
-      
-  }
-  
-  
-  return(df)
+# Da Rulez
+# All decisions are based on the number of occupied seats adjacent
+# to a given seat (one of the eight positions immediately up,
+# down, left, right, or diagonal from the seat)
+# If a seat is empty (L) and there are no occupied seats adjacent to it,
+# the seat becomes occupied.
+# If a seat is occupied (#) and four or more seats adjacent
+# to it are also occupied, the seat becomes empty.
+# Otherwise, the seat's state does not change.
+
+seat_grid <- function(d) {
+  # Encode the seating chart locations
+  tibble(type  = str_split(str_c(d, collapse = ""), pattern = "")[[1]],
+                     row = rep(1:length(d), each = nchar(d)[1]),
+                     column = rep(1:nchar(d)[1],
+                                  times = length(d))) %>%
+    rownames_to_column(var = "seat_number")
 }
 
-update_seats <- function(nb, max_occupied) {
-  out <- nb
-  stable <- FALSE
-  
-  cache <- out
-  while(stable == FALSE) {
-  for (i in seq(1:nrow(cache))) {
-    if(!is.na(cache[i,]$pos)) {
-      occ <- sum(cache[cache[i,]$neighbors[[1]],]$pos, na.rm = T)
-      if(cache[i,]$pos == TRUE) {
-        # This seat is taken. See if the person wants to leave
-        if (occ >= max_occupied) {
-          out[i,]$pos <- FALSE
-        } else {out[i,]$pos <- TRUE}
-      } else if (cache[i,]$pos == FALSE) {
-        # Available seat. Fill if no adjacent seats
-        if(occ == 0) {
-          out[i,]$pos <- TRUE
-        } else {out[i,]$pos <- FALSE}
-      }
-    }
-  }
-    if (all.equal(out$pos, cache$pos) == TRUE) {
-      stable == TRUE
-      return(sum(out$pos, na.rm = T))
-    } else {
-      print(sum(out$pos, na.rm = T))
-      cache <- out
-    }
-  }
+neighboring_seats <- function(seat_map, remove_floor = FALSE) {
+  # Find neighboring/visible seats. Use remove_floor = TRUE for part 2
+}
+
+update_seats <- function(occupied_map, n = 4) {
+  # Process the rules and return a new occupied map
+  # Use n = 5 for part 2
+}
+
+fill_ferry <- function(seat_map, remove_floor = FALSE, n = 4) {
+  # Run seat update until stable and return number of occupied seats
 }
 
 # Question 1 --------------------------------------------------------------
 nb <- find_neighbors(d, mode = "adjacent")
 answer1 <- update_seats(nb, 4)
 answer1
+# 2166 is correct
 
 # Question 2 --------------------------------------------------------------
 nb <- find_neighbors(d, mode = "visible")
---
+++ 2021/6.R
@@ -0,0 +1,59 @@
+library(here)
+library(tidyverse)
+
+#---- read-data ----
+example <- readLines(here("2021", "d6_example.txt"))
+input <- readLines(here("2021", "d6_input.txt"))
+
+init_data <- function(d) {
+  # Split the string on commas and convert to numbers
+  str_split(d, pattern = ',')[[1]] %>%
+    as.numeric()
+}
+
+breed_fish <- function(init_data, days) {
+  # create a table to hold counts of fish at a given timer
+  age_counts <- tibble(ages = 0:8)
+  
+  # initialize with starting counts
+  count_start <- tibble(ages = init_data) %>%
+    group_by(ages) %>%
+    tally()
+  
+  population <- age_counts %>%
+    full_join(count_start, copy = TRUE) %>%
+    replace(is.na(.), 0) %>%
+    mutate(n = as.numeric(n))
+
+  # Rules to update the table
+  update_pop <- function(pop_table) {
+    # Store the number of fish with a zero timer
+    to_breed <- pop_table[[1,2]]
+    
+    # Shift all the timers down 1 day
+    # Then update numbers at position 6 and 8
+    pop_table %>%
+      mutate(n = lead(n)) %>%
+      replace(is.na(.), 0) %>%
+      mutate(n = case_when(
+               ages %in% c(6,8) ~ n + to_breed,
+               TRUE ~ n
+             ))
+  }
+  
+  for (i in 1:days) {
+    population <- update_pop(population)
+  }
+  
+  return(sum(population$n))
+}
+
+#---- part 1 ----
+ie <- init_data(example)
+breed_fish(ie, 80)
+
+ii <- init_data(input)
+breed_fish(ii, 80)
+
+breed_fish(ii, 256) %>%
+  print(digits = 12)

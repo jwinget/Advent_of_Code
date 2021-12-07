ff12e335be494f5c1b5119622aac8da00ef92226
Jason Winget <jwinget@gmail.com>
2021-12-07 09:25:29 -0500

    Day 7 follow up

--- 2021/7.R
+++ 2021/7.R
@@ -64,3 +64,44 @@ rising_cost(example)
 rising_cost(input)
 # 98925156 too high
 # 98925151 is correct
+
+#---- follow-up ----
+# Optimizations after submitting
+# A comment on reddit says:
+# It is possible to compute the exact value target by 
+# adjusting the mean by adding
+# (num_crabs - 2*(num_values_smaller_than_mean))/(2*num_crabs)
+# And then rounding to the nearest integer
+
+rising_cost_2 <- function(d) {
+  # I think now the cheapest is close to the mean instead
+  # But it's not quite that easy...
+  # Just check the integer on either side of the mean
+  # and take the min
+  
+  target_pos <- round(mean(d) +(length(d) - 
+                          2*(length(d[d < mean(d)])))/(2*length(d)))
+  
+  fuel_sum <- function(d, target_pos) {
+    res <- tibble(pos = d, target_pos = target_pos) %>%
+      rowwise() %>%
+      mutate(cost = case_when(
+        # Also need to update the cost calculation
+        pos >= target_pos ~ sum(seq(from = 1,
+                                    to = pos - target_pos)),
+        pos < target_pos ~ sum(seq(from = 1,
+                                   to = abs(pos - target_pos)))
+      ))
+    
+    res %>%
+      pull(cost) %>%
+      sum()
+  }
+  
+  min(unlist(map(target_pos, ~fuel_sum(d, .x))))
+}
+
+rising_cost_2(example)
+rising_cost_2(input)
+
+# Confirming this works
